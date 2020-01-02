a42c7561f43c39d4e4c3aca13ef4cc536daea20b
pjdevs <pj33720@gmail.com>
2020-01-02 15:02:06 +0100

    Add voices select and a lot of words

--- public/js/generator.js
+++ public/js/generator.js
@@ -1,24 +1,36 @@
 var genBtn = document.getElementById("generate");
 var sayBtn = document.getElementById("say");
 var sentenceText = document.getElementById("sentence");
+var voicesSelect = document.getElementById("voices-select");
+
+var voices = window.speechSynthesis.getVoices();
+
+function populateVoices() {
+    for (i = 0; i<voices.length; i++) {
+        var opt = document.createElement("option");
+        opt.innerText = voices[i].name;
+        voicesSelect.appendChild(opt)
+    }
+
+    voicesSelect.selectedIndex = 0;
+}
 
 function say(textToSay) {
     var msg = new SpeechSynthesisUtterance();
-    var voices = window.speechSynthesis.getVoices();
     msg.voiceURI = "native";
     msg.volume = 1;
     msg.rate = 1;
     msg.pitch = 0.8;
     msg.text = textToSay;
     msg.lang = 'fr-FR';
-    msg.voice = voices[0];
+    msg.voice = voices[voicesSelect.selectedIndex];
     speechSynthesis.speak(msg);
 }
 
 genBtn.addEventListener("click", function() {    
     var sentence = (maj(demiPhrase()) + " " + transition() + " " + demiPhrase() + " " + place())
-    .replace(/que un/gi, "qu'un")
-    .replace(/que une/gi, "qu'une");
+    .replace(/ que un/gi, " qu'un")
+    .replace(/ que une/gi, " qu'une");
 
     sentenceText.value = sentence;
 });
@@ -26,3 +38,5 @@ genBtn.addEventListener("click", function() {
 sayBtn.addEventListener("click", function() {
     say(sentenceText.value); 
 });
+
+populateVoices();
